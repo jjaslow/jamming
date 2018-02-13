24ef133d6787859ed1252c896639042a4673351b
Jason Jaslow <jason@jaslow.com>
2018-02-13 07:45:45 -0500

    added onload

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
-import Spotify from '../../util/Spotify';
+import React, { Component } from "react";
+import "./App.css";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
+import Spotify from "../../util/Spotify";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       searchResults: [],
       playlistTracks: [],
       playlistName: "Playlist Name"
-                   };
+    };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
     this.searchSpotify = this.searchSpotify.bind(this);
-    //this.searchSpotify = this.searchSpotify.bind(this);
+    this.getAccessToken = this.getAccessToken.bind(this);
   }
 
-  savePlaylist(){
-    if(this.state.playlistTracks.length > 0)
-      {
-        const trackURIs = this.state.playlistTracks.map(track => 'spotify:track:' + track.id);
-        Spotify.savePlaylist(this.state.playlistName, trackURIs);
-        this.setState(
-          {
-            playlistName: 'Playlist Name',
-            playlistTracks: []
-          }
-        );
-        /*
+  resetAfterSave() {
+    this.setState({
+      playlistName: "Playlist Name"
+    });
+    this.setState({
+      playlistTracks: []
+    });
+  }
+
+  savePlaylist() {
+    if (this.state.playlistTracks.length > 0) {
+      const trackURIs = this.state.playlistTracks.map(
+        track => "spotify:track:" + track.id
+      );
+      Spotify.savePlaylist(this.state.playlistName, trackURIs);
+      this.resetAfterSave();
+      /*
         let spotifyToken = Spotify.getAccessToken();
         let user_id = '';
 
@@ -43,77 +48,80 @@ class App extends Component {
           .then(response => response.json()).then(response=>console.log(response.id));
 
         */
-      }
-
-  }
-
-
-  searchSpotify(query){
-    Spotify.search(query).then(response => this.setState({searchResults: response}));
-    ///////////////////////////////////////////////////////testing
-
-
-  }
-
-
-
-
-addTrack(track){
-     //let searchResultsIds = this.state.searchResults.map(x => x.id);  //get track IDs of songs in Search Results for removing from results when added to playlist
-     //let searchResultsPosition = this.state.searchResults.indexOf(track);  //where in search Results array is selected track?
-
-     let tracks = this.state.playlistTracks;
-     if (tracks.find(x => x.id === track.id))  //is track already in playlist?
-     {
-       return;
-     }
-      tracks.push(track)                                             //mutate track to end of playlist
-      //let searchNew = this.state.searchResults;                     //copy search results
-      //searchNew.splice(searchResultsPosition, 1);                   //remove track from search results
-      this.setState(
-        {
-          playlistTracks: tracks,
-          //searchResults: searchNew
-        });
-      //console.log(this.state.searchResults);
-      //console.log(this.state.playlistTracks);
     }
-
-
-
-
-  removeTrack(track){
-
-     let tracks = this.state.playlistTracks;
-     let playlistPosition = tracks.indexOf(track);  //where in playlist array is selected track?
-
-
-     let playlistNew = tracks;                         //copy playlist
-     playlistNew.splice(playlistPosition, 1);                   //remove track from search results
-     this.setState(
-       {
-         playlistTracks: playlistNew
-       });
-
-
   }
 
+  searchSpotify(query) {
+    Spotify.search(query).then(response =>
+      this.setState({ searchResults: response })
+    );
+    ///////////////////////////////////////////////////////testing
+  }
 
-  updatePlaylistName(name){
+  addTrack(track) {
+    //let searchResultsIds = this.state.searchResults.map(x => x.id);  //get track IDs of songs in Search Results for removing from results when added to playlist
+    //let searchResultsPosition = this.state.searchResults.indexOf(track);  //where in search Results array is selected track?
+
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(x => x.id === track.id)) {
+      //is track already in playlist?
+      return;
+    }
+    tracks.push(track); //mutate track to end of playlist
+    //let searchNew = this.state.searchResults;                     //copy search results
+    //searchNew.splice(searchResultsPosition, 1);                   //remove track from search results
+    this.setState({
+      playlistTracks: tracks
+      //searchResults: searchNew
+    });
+    //console.log(this.state.searchResults);
+    //console.log(this.state.playlistTracks);
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    let playlistPosition = tracks.indexOf(track); //where in playlist array is selected track?
+
+    let playlistNew = tracks; //copy playlist
+    playlistNew.splice(playlistPosition, 1); //remove track from search results
+    this.setState({
+      playlistTracks: playlistNew
+    });
+  }
+
+  updatePlaylistName(name) {
     this.setState({ playlistName: name });
-      }
+  }
 
+  componentDidMount() {
+    window.addEventListener("load", this.getAccessToken);
+  }
 
+  getAccessToken() {
+    let token = Spotify.getAccessToken();
+    console.log(token);
+  }
 
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
         <div className="App">
           <SearchBar onSearch={this.searchSpotify} />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistTracks={this.state.playlistTracks} playlistName={this.state.playlistName} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistTracks={this.state.playlistTracks}
+              playlistName={this.state.playlistName}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
+            />
           </div>
         </div>
       </div>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import './SearchBar.css';
+import React from "react";
+import "./SearchBar.css";
 
 class SearchBar extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {term: ''};
-    this.searchSpotify =  this.searchSpotify.bind(this);
+    this.state = { term: "" };
+    this.searchSpotify = this.searchSpotify.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
 
-  searchSpotify(){
-    this.props.onSearch(this.state.term)
+  searchSpotify() {
+    this.props.onSearch(this.state.term);
   }
 
-  handleTermChange(event){
-    this.setState({term: event.target.value});
+  handleTermChange(event) {
+    this.setState({ term: event.target.value });
   }
 
-
-  render(){
-    return(
+  render() {
+    return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+        <input
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleTermChange}
+        />
         <a onClick={this.searchSpotify}>SEARCH</a>
       </div>
-    )
-
-
-
+    );
   }
-
-
-};
-
-
+}
 
 export default SearchBar;
